b42aa35d50b157d06e2b779376443d8ab535b0c9
-=V.I.M.E.R=- <top.michan@mail.ru>
2025-10-17 18:02:58 +0500

    1

--- src/module/webSocket/index.ts
+++ src/module/webSocket/index.ts
@@ -1,7 +1,8 @@
-import { randomInt } from "crypto";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import weatherService from "../../app/service/weatherService.js";
 let io: Server | null = null;
+
+let socketClient = new Set<Socket>();
 
 function webSocketInit(server: any) {
   io = new Server(server, {
@@ -12,22 +13,27 @@ function webSocketInit(server: any) {
   });
   io.on("connection", (socket) => {
     console.log(`🟢 Подключение клиента: ${socket.id}`);
+    socketClient.add(socket);
+
+    socket.on("disconnect", () => {
+      socketClient.delete(socket);
+    });
     // socket.emit("weatherCurrent", weatherService.CurrentWeather);
-    setInterval(async () => {
-      const date = new Date();
-      console.log(date.getTime());
-      if (
-        weatherService.last_updated_epoch_Current == null ||
-        weatherService.last_updated_epoch_Current * 1000 + 900000 < date.getTime()
-      ) {
-        const CurrentWeather = await weatherService.getCurrentWeather();
-        if (CurrentWeather) return socket.emit("weatherCurrent", CurrentWeather);
-      }
-      console.log(
-        weatherService.last_updated_epoch_Current ? weatherService.last_updated_epoch_Current * 1000 + 900000 : null
-      );
-      socket.emit("weatherCurrent", weatherService.CurrentWeather);
-    }, 5000);
+    // setInterval(async () => {
+    //   const date = new Date();
+    //   console.log(date.getTime());
+    //   if (
+    //     weatherService.last_updated_epoch_Current == null ||
+    //     weatherService.last_updated_epoch_Current * 1000 + 900000 < date.getTime()
+    //   ) {
+    //     const CurrentWeather = await weatherService.getCurrentWeather();
+    //     if (CurrentWeather) return socket.emit("weatherCurrent", CurrentWeather.data);
+    //   }
+    //   console.log(
+    //     weatherService.last_updated_epoch_Current ? weatherService.last_updated_epoch_Current * 1000 + 900000 : null
+    //   );
+    //   socket.emit("weatherCurrent", weatherService.CurrentWeather.data);
+    // }, 5000);
 
     socket.on("join_room", async (roomId: string) => {
       socket.join(roomId);
@@ -36,9 +42,17 @@ function webSocketInit(server: any) {
     });
   });
 }
+function gg() {
+  if (socketClient) {
+    for (const item of socketClient) {
+      item.emit("weatherCurrent", "dddd");
+    }
+  }
+}
+
 function getIO(): Server {
   if (!io) throw new Error("Socket.io не инициализирован");
   return io;
 }
 export default getIO;
-export { webSocketInit };
+export { webSocketInit, gg };
