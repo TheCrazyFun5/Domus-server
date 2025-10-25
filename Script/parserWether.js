8e7889001a326aa7da08f866d3477a057b335b65
-=V.I.M.E.R=- <top.michan@mail.ru>
2025-10-25 11:57:44 +0500

    parserDone

--- Script/parserWether.js
+++ Script/parserWether.js
@@ -1,11 +1,11 @@
-import { accessSync, constants, readFileSync } from "fs";
+import { accessSync, writeFileSync, readFileSync } from "fs";
 
 const fileContent = readFileSync("./conditions.json", "utf-8");
 const contentWithoutBOM = fileContent.replace(/^\uFEFF/, "");
-const data = JSON.parse(contentWithoutBOM);
-
+let data = JSON.parse(contentWithoutBOM);
 if (Array.isArray(data)) {
   for (const item of data) {
-    console.log(item);
+    item.languages = item.languages.filter((item, index, array) => item.lang_iso == "ru");
   }
 }
+writeFileSync("./conditionsOut.json", JSON.stringify(data, null, 2), "utf-8");
